37ffbd5f457ffde3da2f76ddf0579ec6bb86d80e
WinterGrave <paulmongeurquijo@gmail.com>
2018-01-22 20:08:13 -0700

    0.0.3-alpha
    -Now it says the correct name depending on the item block
    -It now supports entities
    -Added support for water and lava

--- src/main/java/zairus/squirrelguide/SGConstants.java
+++ src/main/java/zairus/squirrelguide/SGConstants.java
@@ -4,7 +4,7 @@ public class SGConstants
 {
 	public static final String MOD_ID = "squirrelguide";
 	public static final String MOD_NAME = "SquirrelGuide";
-	public static final String MOD_VERSION = "0.0.2-alpha";
+	public static final String MOD_VERSION = "0.0.3-alpha";
 	
 	public static final String COMMON_PROXY = "zairus." + MOD_ID + ".SGProxy";
     public static final String CLIENT_PROXY = "zairus." + MOD_ID + ".client.SGProxyClient";
--- src/main/java/zairus/squirrelguide/event/SGEventHandler.java
+++ src/main/java/zairus/squirrelguide/event/SGEventHandler.java
@@ -1,9 +1,20 @@
 package zairus.squirrelguide.event;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.chat.NarratorChatListener;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -29,45 +40,105 @@ public class SGEventHandler
 		{
 			Minecraft mc = Minecraft.getMinecraft();
 			World world = mc.world;
-			RayTraceResult rtr = mc.objectMouseOver;
+			EntityPlayer player = mc.player;
 			
-			if (world != null && rtr != null && rtr.typeOfHit == RayTraceResult.Type.BLOCK && rtr.getBlockPos() != null)
+			if (world == null)
+				return;
+			
+			if (player == null)
+				return;
+			
+			float f = player.rotationPitch;
+			float f1 = player.rotationYaw;
+			double d0 = player.posX;
+			double d1 = player.posY + (double)player.getEyeHeight();
+			double d2 = player.posZ;
+			Vec3d vec3d = new Vec3d(d0, d1, d2);
+			float f2 = MathHelper.cos(-f1 * 0.017453292F - (float)Math.PI);
+			float f3 = MathHelper.sin(-f1 * 0.017453292F - (float)Math.PI);
+			float f4 = -MathHelper.cos(-f * 0.017453292F);
+			float f5 = MathHelper.sin(-f * 0.017453292F);
+			float f6 = f3 * f4;
+			float f7 = f2 * f4;
+			double d3 = player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue();
+			Vec3d vec3d1 = vec3d.addVector((double)f6 * d3, (double)f5 * d3, (double)f7 * d3);
+			
+			boolean stopOnLiquid = true;
+			boolean ignoreBlockWithoutBoundingBox = false;
+			boolean returnLastUncollidableBlock = false;
+			
+			RayTraceResult rtr = world.rayTraceBlocks(vec3d, vec3d1, stopOnLiquid, ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock); // mc.player.rayTrace(5.0D, 0.0F);
+			RayTraceResult rtr_ent = mc.objectMouseOver;
+			
+			String say = "";
+			ITextComponent itextcomponent = null;
+			
+			if (rtr_ent != null)
 			{
-				Block block = world.getBlockState(rtr.getBlockPos()).getBlock();
-				
-				String say = block.getLocalizedName();
-				
-				say = I18n.translateToFallback(say);
-				
-				if (I18n.canTranslate(say))
-					say = I18n.translateToLocal(say);
-				
-				if (say.contains("bed"))
-					say = "Bed";
-				
-				if (!say.contains(".") && say != lastName)
+				if (rtr_ent.typeOfHit == RayTraceResult.Type.ENTITY && rtr_ent.entityHit != null)
 				{
-					lastName = say;
+					Entity entity = rtr_ent.entityHit;
 					
-					ITextComponent itextcomponent = new TextComponentTranslation(lastName);
-					//ITextComponent itextcomponent1 = new TextComponentString("nar");
-					//mc.world.getMinecraftServer().sendMessage(itextcomponent);
-					//itextcomponent.appendSibling(itextcomponent1);
-					//mc.player.sendChatMessage(say);
-					//mc.player.sendStatusMessage(itextcomponent, true);
-					//mc.player.sendMessage(itextcomponent);
-					//mc.ingameGUI.getChatGUI().printChatMessage(itextcomponent);
-					
-					//Narrator narrator = Narrator.getNarrator();
-					//narrator.clear();
-					NarratorChatListener.INSTANCE.clear();
-					mc.ingameGUI.addChatMessage(ChatType.SYSTEM, itextcomponent);
-					//narrator.say(say);
+					itextcomponent = entity.getDisplayName();
+					say = itextcomponent.getFormattedText();
 				}
-				else
+			}
+			
+			if (rtr != null)
+			{
+				if (rtr.typeOfHit == RayTraceResult.Type.BLOCK && rtr.getBlockPos() != null)
 				{
-					lastName = say;
+					IBlockState blockState = world.getBlockState(rtr.getBlockPos());
+					Block block = blockState.getBlock();
+					
+					ItemStack stack = new ItemStack(Item.getItemFromBlock(block), 1, block.getMetaFromState(blockState));
+					
+					List<String> dataList = stack.getTooltip((EntityPlayer)null, ITooltipFlag.TooltipFlags.NORMAL);
+					
+					if (dataList.size() > 0 )
+					{
+						say = dataList.get(0).replace("§r", "");
+					}
+					else
+					{
+						say = Item.getItemFromBlock(block).getItemStackDisplayName(new ItemStack(block)); //block.getLocalizedName();
+						
+						say = I18n.translateToFallback(say);
+						
+						if (I18n.canTranslate(say))
+							say = I18n.translateToLocal(say);
+					}
+					
+					if (blockState.getMaterial() == Material.WATER)
+						say = "Water";
+					
+					if (blockState.getMaterial() == Material.LAVA)
+						say = "Lava";
+					
+					if (block.getLocalizedName().contains("bed"))
+						say = "Bed";
+					
+					itextcomponent = new TextComponentTranslation(say);
 				}
+			}
+			
+			if (itextcomponent != null && !say.equals(lastName))
+			{
+				lastName = say;
+				
+				//ITextComponent itextcomponent = new TextComponentTranslation(lastName);
+				//ITextComponent itextcomponent1 = new TextComponentString("nar");
+				//mc.world.getMinecraftServer().sendMessage(itextcomponent);
+				//itextcomponent.appendSibling(itextcomponent1);
+				//mc.player.sendChatMessage(say);
+				//mc.player.sendStatusMessage(itextcomponent, true);
+				//mc.player.sendMessage(itextcomponent);
+				//mc.ingameGUI.getChatGUI().printChatMessage(itextcomponent);
+				
+				//Narrator narrator = Narrator.getNarrator();
+				//narrator.clear();
+				NarratorChatListener.INSTANCE.clear();
+				mc.ingameGUI.addChatMessage(ChatType.SYSTEM, itextcomponent);
 			}
 		}
 	}
